6bdf98d5cd96952567f78c28b0f9112d038a657b
maruf-hussain <engmarufhussain@gmail.com>
2023-08-26 16:50:21 +0600

    ajfa

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBi5wteDACtD5bIlDBmFJQDPLemztlr1p8",
+  authDomain: "ema-jhon-ffe0c.firebaseapp.com",
+  projectId: "ema-jhon-ffe0c",
+  storageBucket: "ema-jhon-ffe0c.appspot.com",
+  messagingSenderId: "77590932746",
+  appId: "1:77590932746:web:3ebe024e47d13cecac1ef3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import logo from '../../images/Logo.svg';
 import './Header.css'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav className='header'>
         <img  src={logo} alt="" />
         <div>
-         <a href="/">Shop</a>
-        <a href="/orders">Orders</a>
-        <a href="/inventory">Inventory</a>
-        <a href="/login">Login</a>
+        <Link to="/orders">Orders</Link>
+        <Link to="/inventory">Inventory</Link>
+        <Link to="/">Shop</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/signup" >Sign Up</Link>
         </div>
      </nav>
     );
